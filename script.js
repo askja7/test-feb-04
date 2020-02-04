c8de53a9ef000ba357ac554c7e6479c0ad6c78c3
lorixx <staneva.lora@gmail.com>
2020-02-04 10:47:20 +0100

    bubbles

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function start() {
     const bubbles = document.querySelectorAll("#wrapper>div");
     console.log(bubbles)
     bubbles.forEach(bubble => {
-        const x = Math.random() * 100;
-        const y = Math.random() * 100;
-        const hue = Math.random() * 360;
-        bubble.style.background = "hsl(120, 70%, 70%)";
-        bubble.style.transform="translate("+x+"50vw,"+y+"50vh)";
+        const x = Math.random()*100;
+        const y = Math.random()*100;
+        const hue = Math.random()*360;
+        bubble.style.background = "hsl("+hue+", 70%, 70%)";
+        bubble.style.transform="translate("+x+"vw, "+y+"vh)";
     })
 }
